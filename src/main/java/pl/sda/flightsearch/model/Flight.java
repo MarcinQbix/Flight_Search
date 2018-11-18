6bfe5ea84e4a77d8adef20432d4412d34342e336
Dorota Gołębiowska <golebiowska.doro@gmail.com>
2018-11-18 12:21:33 +0100

    chujowo ale stabilnie

--- src/main/java/pl/sda/flightsearch/model/Flight.java
+++ src/main/java/pl/sda/flightsearch/model/Flight.java
@@ -1,10 +1,13 @@
 package pl.sda.flightsearch.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.Date;
-
+@Getter
+@Setter
 public class Flight {
     private String flyTo;
 
