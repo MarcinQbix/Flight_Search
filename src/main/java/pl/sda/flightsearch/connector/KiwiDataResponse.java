136ae0aa0060b2d55de273e7ae32e3ce24f74c28
Dorota Gołębiowska <golebiowska.doro@gmail.com>
2018-11-18 12:19:21 +0100

    chujowo ale stabilnie

--- src/main/java/pl/sda/flightsearch/connector/KiwiDataResponse.java
+++ src/main/java/pl/sda/flightsearch/connector/KiwiDataResponse.java
@@ -1,11 +1,20 @@
 package pl.sda.flightsearch.connector;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
+import pl.sda.flightsearch.model.Flight;
+
+import java.util.List;
 
 @Getter
 @Setter
 @Component
 public class KiwiDataResponse {
+
+    private String currency;
+
+    @JsonProperty("data")
+    private List<Flight> flights;
 }
--- src/main/java/pl/sda/flightsearch/model/Flight.java
+++ src/main/java/pl/sda/flightsearch/model/Flight.java
@@ -1,4 +1,35 @@
 package pl.sda.flightsearch.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class Flight {
+    private String flyTo;
+
+    private String airlines;
+
+    @JsonProperty("fly_duration")
+    private String flyDuration;
+
+    private String flyFrom;
+
+    @JsonProperty("dTimeUTC")
+    private Date dTimeUTC;
+
+    @JsonProperty("booking_token")
+    private String bookingToken;
+
+    private String cityTo;
+
+    private String cityFrom;
+
+    @JsonProperty("aTimeUTC")
+    private Date aTimeUTC;
+
+    private BigDecimal price;
+
+
+
 }
