e1bd3b44fd402b168ca958b1d2754be5c0b53c9a
Marcin Kubiczek <kubiczekmar@gmail.com>
2018-11-18 11:49:08 +0100

    update

---
+++ src/main/java/pl/sda/flightsearch/connector/KiwiDataResponse.java
@@ -0,0 +1,11 @@
+package pl.sda.flightsearch.connector;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+public class KiwiDataResponse {
+}
---
+++ src/main/java/pl/sda/flightsearch/model/Flight.java
@@ -0,0 +1,4 @@
+package pl.sda.flightsearch.model;
+
+public class Flight {
+}
