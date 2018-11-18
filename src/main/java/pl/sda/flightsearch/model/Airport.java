fe3b892d565d3cb0ddeb6af9cbaaf4622515a54a
Marcin Kubiczek <kubiczekmar@gmail.com>
2018-11-18 14:55:01 +0100

    DOJEBANE

--- src/main/java/pl/sda/flightsearch/model/Airport.java
+++ src/main/java/pl/sda/flightsearch/model/Airport.java
@@ -1,4 +1,15 @@
 package pl.sda.flightsearch.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
 public class Airport {
+    private String airportCode;
+    private String aiportTown;
+    private String airportCountry;
 }
